9904157796866b1f28b37cca56f6d1b319f847d5
Elise Lee <eliselee2021@u.northwestern.edu>
2021-03-01 17:48:11 -0600

    add intermediate data

--- scripts/alexaWebScraper.py
+++ scripts/alexaWebScraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 
 # Scrapes Alexa site for Top 500 URLS from 20 out of 123 countries
 class AlexaScraper():
@@ -32,6 +33,7 @@ class AlexaScraper():
     def getTop500ForCountry(self, countryCode, cookies):
         countrySites = []
         for pageNum in range(20):
+            time.sleep(2)
             URL = f"{self.baseURL}countries;{pageNum}/{countryCode}"
             page = requests.get(
                 f"{self.baseURL}countries;{pageNum}/{countryCode}",
@@ -45,52 +47,50 @@ class AlexaScraper():
                     print(site)
                 else:
                     print("DUPE")
+        return countrySites
 
 def convertDictToJson(dict, jsonFileName):
     with open(jsonFileName, "w") as outfile:
         json.dump(dict, outfile, indent = 4)
 
-def convertRawScrapedDataToDict():
-    countryURLDict = {}
+def joinFiles():
+    dict = {}
+    files = [
+        "alexaTop500SitesRegional0_10.json",
+        "alexaTop500SitesRegional11_37.json",
+        "alexaTop500SitesRegional.json"
+    ]
+    for f in files:
+        with open(f, "r") as infile:
+            f_dict = json.load(infile)
+            dict.update(f_dict)
+    convertDictToJson(dict, "alexaTop500SitesCountries.json")
 
-    with open("../data/countryCodesToNames.json", "r") as infile:
-        countryCodesDict = json.load(infile)
-
-    fileObj = open("../data/rawScrapedAlexaData_AfghanistanToCameroon.json", "r")
-    URLS = fileObj.read().splitlines()  # puts the file into an array
-    fileObj.close()
-
-    for idx, countryCode in enumerate(list(countryCodesDict.keys())):
-        offset = idx * 500
-        if offset + 500 <= len(URLS):
-            countryURLDict[countryCode] = URLS[offset:offset + 500]
-        else:
-            break
-
-    return countryURLDict
 
 def runProgram():
+   # joinFiles()
+
    # Hardcodes cookies to scrape top 500 URLS for each country. Stops working after 20 countries.
    # Must hardcode your own cookies (after logging in)
    cookies = {
-       "attr_first": "%7B%22source%22%3A%22(direct)%22%2C%22medium%22%3A%22(none)%22%2C%22campaign%22%3A%22(not%20set)%22%2C%22term%22%3A%22(not%20provided)%22%2C%22content%22%3A%22(not%20set)%22%2C%22lp%22%3A%22www.alexa.com%2Ftopsites%2Fglobal%3B5%22%2C%22date%22%3A%222021-02-9%22%2C%22timestamp%22%3A1612916553026%7D",
-       "session_key": "0feIaZWxtZQ%2BUPG%2FLH48WTfEXkyiNdff68YJjFLsJx57XlwjJgPhE%2BLLGxNl51nRfsEtwJ5ckJkxTuP1B%2BYeWdJZ49thPNSAsdS27ZyvpH9ni4OgiyXjchB4QiBQCbtIF7wEZPca0UlFNU16D8KGcu32l7SjUrOrJkTHiesjmzJLgDhrBsFAqXzpEANoM5qXla43ficU%2BZfQ7Sb2ErLdtPYMiLBimMRiWKK48jufASg%3D",
-        "_gat_UA-2146411-12": "1",
-       "session_www_alexa_com": "479d7553-3253-4617-97ca-329042886b63",
-       "session_www_alexa_com_daily": "1614387595",
-       "jwtScribr": "eJyrViopVrIyNDM0MTE1sTAw0bMw0VHKzQEKGRjoKBVnpgBllcwtTS3MzPUSU8oS85JTU5RAEiWpIBmgKj0gNxnGV6oFAFjuFa4%3D.LFmALFWNfVueJqlS5lvFe_HboWOyc1GWDpUX14WdDJQ",
+        "session_key": "gOc7EFh0%2Bctah5oMrngehl7ND2wdP8TkQArghbsJ01NETC%2F1L9%2Bjz5GT%2Fvc2qtf%2B5Jmt4TnHo0I8ONxw6kIxGDNLwnXrYv7NLUzY9i4eG1IDLO4XNw0hz3WdKp5hbg%2FubMM6jiM68Kpnx5r9zPmL%2BUdy97mAAbnukOS%2BGYGt3syZZIrceE5ksRZ70pO300RFwiOMQKrwqBVBOHTQWi%2FubCQaZInWu%2FJ31435NOCJGck%3D",
+        "session_www_alexa_com": "479d7553-3253-4617-97ca-329042886b63",
+        "jwtScribr": "eJyrViopVrIyNDM0MTM2MTc01DMzM9VRys0BihkY6CgVZ6YApZXMLU0tzMz1ElPKEvOSU1OUQBIlqSAZoCo9IDcZxleqBQBprxXg.iCgWlaFSdrNN02jEO0ISQIAz_zwKVQeDP1q8HZ7h2YI",
    }
    alexaScraper = AlexaScraper()
    alexaScraper.getAllCountries()
    convertDictToJson(alexaScraper.countryNames, 'countryCodesToNames.json')
    countryCodes = alexaScraper.countryNames.keys()
-   for countryCode in countryCodes[:20]:
-       alexaScraper.countryURLS[countryCode] = alexaScraper.getTop500ForCountry(countryCode, cookies)
-   convertDictToJson(alexaScraper.countryURLS, "alexaTop500Sites_AfghanistanToCameroon.json")
+   for countryCode in list(countryCodes)[38:]:
+       try:
+           alexaScraper.countryURLS[countryCode] = alexaScraper.getTop500ForCountry(countryCode, cookies)
+       except Exception as e:
+           convertDictToJson(alexaScraper.countryURLS, "alexaTop500SitesRegional.json")
+           print(e)
+           return
+   convertDictToJson(alexaScraper.countryURLS, "alexaTop500SitesRegional.json")
 
-  # #  Used to convert some raw scraped data into a JSON of country codes to URLS after Amazon blocked us
-  #  countryURLDict = convertRawScrapedDataToDict()
-  #  convertDictToJson(countryURLDict, "alexaTop500Sites_AfghanistanToCameroon.json")
+
 
 if __name__ == "__main__":
     runProgram()
